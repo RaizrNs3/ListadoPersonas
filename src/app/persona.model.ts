b2f07ecc2ad36992ca48c8b1de7c8130708a5b3b
Daniel <daniel.lopezing20@gmail.com>
2021-02-24 10:01:06 -0600

    Corrección en el funcionamiento del botón agregar persona.

---
+++ src/app/persona.model.ts
@@ -0,0 +1,13 @@
+export class Persona{
+  /*nombre = '';
+  apellido = '';
+
+  constructor(nombre: string, apellido: string) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+  }*/
+
+  /* Declarar e inicializar los atributos en el mismo constructor: */
+  constructor(public nombre: string, public apellido: string) { }
+
+}
